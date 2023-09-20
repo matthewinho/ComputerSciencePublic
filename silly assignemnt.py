86957c03cb84498ff24fdb342b5e9faff7f64bab
matthewinho <144443687+matthewinho@users.noreply.github.com>
2023-09-20 13:12:56 +0100

    Add files via upload

---
+++ silly assignemnt.py
@@ -0,0 +1,40 @@
+#list1 = ["football","basketball"]
+
+
+#gk = input("Whats your favourite sport?")
+
+
+
+#list1.append(gk)
+
+#print(list1)
+
+
+#TypeError: can only concatenate str (not "list") to str
+
+
+"""list2 = ["Maths" , "Geography" ,  "Biology" , "Irish" , "History"  , "English"]
+print(list2)
+
+A = input("What subjects do you not like?")
+
+list2.remove(A)
+
+
+print(list2)"""
+
+
+list4 = ["red" , "blue" , "purple" , "yellow" , "green" , "grey" , " pink " , "gold" , "silver" , "white" ]
+
+
+sm = input(" pick a number between 0-4")
+
+sm1 = int(sm)
+
+print(sm)
+ks2 = int(ks)
+
+ks = input( "pick number between 5-9" )
+
+print(sm1)
+
