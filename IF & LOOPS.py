be97e666124cb1f08c59c0d59bb26900b70db3ae
matthewinho <144443687+matthewinho@users.noreply.github.com>
2023-09-29 12:34:50 +0100

    Add files via upload

---
+++ IF & LOOPS.py
@@ -0,0 +1,76 @@
+"""for i in [1,2,3]:
+    print("hello")
+    
+    
+    
+for h in [ 1,1,1,1,1,1,1,1,1,1]:
+    print("ciicii")
+    """
+
+#12
+
+"""a = int(input("give me a number "))
+
+b = int(input("give me a second number "))
+
+
+
+if a > b:
+    print(b,"and",a )
+    
+if  a < b:
+    print(a, "and" ,b)"""
+    
+""" #13   
+r = int(input("Enter a number that is below 20 "))
+
+if r > 20:
+    print("too HIGH!!")
+    
+if r == 20 or r < 20:
+     
+    print("Thank you")      """
+
+""" #14
+h =int(input("Enter a number between 10-20"))
+a = (10) 
+if h == 20 or a < 20:
+    print("Thank you") 
+    
+if h > a:
+    print("Incorrect!")
+    
+#if h > 20:
+   # print("Incorrect!") """
+   
+   
+   
+"""#15
+b = "red"
+c = "RED"
+wa = (input("whats your favourite colour "))
+if wa == b or wa == c:
+       print(" I like red too")
+else:
+    print("I dont like", wa , " i prefer red")  """
+
+
+
+#17
+"""lo = (input("Are you 18 or over? "))
+
+poo = (17)
+a = (18)
+if lo >18 :
+    print("You can vote")"""
+    
+    
+    
+#19
+"""A = int(input("ENTER 1 OR 2 Or 3 "))
+if A == 1:
+    print("thank you")
+if A == 2:
+    print("well done")
+if A == 3:
+    print("correct")"""
