d440382c4b3331c7aa5860b996dedd3b797d6bb4
matthewinho <144443687+matthewinho@users.noreply.github.com>
2023-10-06 12:29:52 +0100

    Add files via upload

---
+++ assignment 2nd of OCT = WHILE LOOP CHALLENGES.py
@@ -0,0 +1,88 @@
+#while is true = run code, if not true code doesnt RUN
+
+
+"""total = 0
+
+while total == 50 or total < 50:
+    o = int(input("Add a number  "))
+    total = total + o
+    print(total)
+    print("The total is" ,o)
+if o > 50:"""
+    # print("The TOTAL IS OVER 50")
+#while a < 5:
+"""a = int(input("Enter a number"))
+b = 0
+if a < 5:
+    print("Enter a number", b+ a)"""
+    
+    #47
+#p  = 0
+#i = 0
+#a  = int(input("Enter a number "))
+#b = int(input("Enter another number "))
+#lol = true
+#T = a+b
+#print(T)
+"""while lol:
+       u =     int(input("another number?? y/n :  "))
+       if u == "y":
+           b= int(input("your next number is going to be added"))
+           print("added it equals..")
+           print(u+T)
+        #int(input("another number"))
+           if u == "n":
+                 break"""
+             
+#49
+
+
+
+
+
+
+
+#50
+"""A = True
+
+ 
+
+while A == True:
+    B =int(input("ENTER A NUMBER "))
+    if B < 10:
+        print("TOO LOW")
+    if B > 20:
+        print("TOO HIGH")
+    if B >= 10 and B <= 20:
+        print("THANK YOU")"""
+#51
+a = 10
+d  = a - 1
+#knoc
+while  True:
+           
+           print("there are" , a , "green bottles hanging on the wall, and if 1 green bottle should accidentally fall " )
+           
+           d  = a -1 
+           b = int(input("there will be ? green bottles sitting on a wall  "))
+           if b == d:
+               a = a -1
+            
+                #print(d ,"left")
+           if b < d or b > d:
+                print(" NUH UH. try again")
+           if a == 0:
+               print("CONGRATULATIONS ALL THE BOTTLES WERE KNOCKED DOWN")
+               break
+                              
+
+   
+
+
+
+
+
+
+
+
+
