475ef8576f0559a736c4f88e3ca11f98e6960277
matthewinho <144443687+matthewinho@users.noreply.github.com>
2023-12-11 18:41:53 +0000

    Add files via upload

---
+++ 70CT.py
@@ -0,0 +1,91 @@
+
+#1
+
+"""
+
+for i in range (1,11):
+
+    print(i)
+
+
+#2
+
+
+for a in range(20,0,-1):
+
+    print(a)
+
+
+ 
+ 
+
+ 
+ 
+ 
+#6
+
+for h  in  [1,2,3,4,5]:
+
+   print("happy birthday")
+"""  
+   
+"""
+#3
+   
+
+for num in range(1, 11):
+    if num % 2 == 0:
+        print(num)
+        
+        
+#4
+        
+        
+n = 20
+
+
+for i in range(1,n):
+     print(i)
+"""    
+#5
+"""
+n = 20
+for z in range(1,n,2):
+    print(z)
+"""
+
+#7
+"""
+n = int(input("Enter a number: "))
+
+print("The first", n, "terms of the series are:", end=" ")
+for i in range(1, n+1):
+    print(i**2, end=" ")
+"""       
+#8
+    
+"""   
+a = int(input("Enter a number: "))
+
+print("Multiplication table for", a)
+for i in range(1, 13):
+    print(a, "x", i, "=", a * i)
+    
+    
+#9
+    
+for g in range(3,32,4):
+    print(g)
+"""
+#10
+v = 2
+ratio= 3
+
+
+for i in range(6):
+    XD =   v * (ratio ** i)
+    print(XD)
+
+
+
+
