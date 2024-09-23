40a9aa46ed314dbdbc087537ce4ae8ff15c6af19
matthewinho <144443687+matthewinho@users.noreply.github.com>
2024-09-23 11:57:58 +0100

    Update FREQUENCY.py

--- 6thYEAR/FREQUENCY.py
+++ 6thYEAR/FREQUENCY.py
@@ -1,15 +1,12 @@
-L = [int(item) for item in input("Enter a list with spaces in between :").split()]
-a = -1
-checked = []
-#a = a +L[a]
-n = len(L)
-for i in range (0,n):
-    if L[i] not in checked:
-        checked.append(L[i])
-        a = a +1
-        b = L[a]
-    #print(b)
-        test = L.count(b)
-        test1 = b<1
-    
-        print(b, "occurs" , test, "amount of times")
+def freq(L):
+    check = []
+    n = len(L)
+    for i in range (0,n):
+        if L[i] not in check:
+            check.append(L[i])
+            b = L[i]
+            frequency = L.count(b)
+            print(b, "occurs" , frequency, "amount of times")
+ 
+ 
+Awns =freq(L)
