f953725eeeaf71aa12bf6824ec00faa2d105ca4d
matthewinho <144443687+matthewinho@users.noreply.github.com>
2024-09-27 12:28:50 +0100

    Add files via upload

---
+++ 6thYEAR/assignment27.py
@@ -0,0 +1,17 @@
+L = [[2,5,0],[3,7,4],[1,6,9],[4,2,0]] #list within lists
+print(L) #wholething
+print(L[0]) #calls first set of numbers in list
+# ROWS AND COLUMNS START AT 0 
+# to print number 6
+row6 = L[2][1]
+print(row6)
+
+
+
+#MY 2D DICTIONARY
+
+D2  ={'John':{'N':3056 ,'S':8463,'E':8441,'W':2694},'Tom':{'N':3056 ,'S':8463,'E':8441,'W':2694}}
+
+
+print(D2['John'])
+print(D2['John']['N'])
